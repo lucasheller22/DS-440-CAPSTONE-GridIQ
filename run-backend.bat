d9bcdbc38c968a91987c233e19c2ff19d40037c5
lrheller03 <lucasheller22@gmail.com>
2026-03-29 16:13:49 -0400

    removing junk

--- run-backend.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Start the API from gridiq-backend (avoids ModuleNotFoundError: No module named 'app')
-cd /d "%~dp0gridiq-backend"
-uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
